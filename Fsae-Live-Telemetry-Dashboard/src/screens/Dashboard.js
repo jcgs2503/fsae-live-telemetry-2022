3b804f4e51aecf9899f58f5bacc8f52be0924e6a
Jerry Chang <jcgs2503@gmail.com>
2021-09-29 19:43:22 -0500

    bugs

--- Fsae-Live-Telemetry-Dashboard/src/screens/Dashboard.js
+++ Fsae-Live-Telemetry-Dashboard/src/screens/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useData } from "../contexts/DataContext";
 import { JsonToTable } from "react-json-to-table";
 import { Button, Form } from "react-bootstrap";
@@ -24,22 +24,44 @@ const Table = styled.div`
 export default function Dashboard() {
 	const { currentData } = useData();
 	const options1 = Object.keys(currentData);
+	const [dataGroup, setDataGroup] = useState(options1[0]);
+
+	// const options2 = Object.keys(currentData[options1[0]]);
+	// const [dataNum, setDataNum] = useState(options2[0]);
+
 	return (
 		<>
 			<Select>
-				<SelectOption as="select">
-					<option>1</option>
-					<option>2</option>
+				<SelectOption
+					as="select"
+					onChange={(e) => {
+						setDataGroup(e.currentTarget.value);
+					}}
+				>
+					{options1.map((op) => (
+						<option value={op} key={op}>
+							{op}
+						</option>
+					))}
 				</SelectOption>
-				<SelectOption as="select">
-					<option>1</option>
-					<option>2</option>
-				</SelectOption>
+				{/* <SelectOption
+					as="select"
+					onChange={(e) => {
+						setDataNum(e.currentTarget.value);
+					}}
+				>
+					{options2.map((op) => (
+						<option value={op} key={op}>
+							{op}
+						</option>
+					))}
+				</SelectOption> */}
 			</Select>
 
 			<Table>
-				<JsonToTable json={currentData} />
+				<JsonToTable json={currentData[dataGroup]} />
 			</Table>
+			{options1}
 		</>
 	);
 }
