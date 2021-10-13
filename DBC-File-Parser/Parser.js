141937b46c5170b0d13cec25615271eb7ab292ec
Jerry Chang <jcgs2503@gmail.com>
2021-10-13 03:10:34 -0500

    finished dbc file parser

--- DBC-File-Parser/Parser.js
+++ DBC-File-Parser/Parser.js
@@ -1,1 +1,6 @@
-import { transmutator } from "@viriciti/dbc-to-json";
+const dbcToJson = require("@viriciti/dbc-to-json");
+const fs = require("fs");
+
+const dbcstring = fs.readFileSync("./can-configuration/config.dbc", "UTF-8");
+const json = dbcToJson(dbcstring);
+fs.writeFileSync("./config.json", JSON.stringify(json));
