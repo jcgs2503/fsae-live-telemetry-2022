0d187ff1e2247bd9d9a68b14202e6358d45c9b58
Jerry Chang <jcgs2503@gmail.com>
2022-02-10 17:08:10 -0600

    finished big endian

--- Fsae-Live-Telemetry-Dashboard/src/lib/canParser.js
+++ Fsae-Live-Telemetry-Dashboard/src/lib/canParser.js
@@ -2,14 +2,17 @@ const data = require("../contexts/config.json");
 var hexToBinary = require("hex-to-binary");
 
 function hex2binary(hex) {
-	let modifiedHex = `1${hex}`;
-	let convertedBin = hexToBinary(modifiedHex);
-	let slicedBin = convertedBin.slice(1, -1);
-	return slicedBin;
+	let convertedBin = hexToBinary(hex);
+	return convertedBin;
+}
+
+function reverseString(str) {
+	return str.split("").reverse().join("");
 }
 
 function canParser(id, hexString) {
 	let inputData = hex2binary(hexString);
+	console.log(inputData);
 	const node = data.params.filter((e) => e.canId === id)[0];
 	const signals = node.signals.map((e) => e);
 
@@ -18,12 +21,18 @@ function canParser(id, hexString) {
 	signals.map((e) => {
 		if (e.isLittleEndian) {
 		} else {
-			let result = inputData.slice(e.startBit, e.startBit + e.bitLength);
-			// problems
+			let count = Math.floor(e.bitLength / 8);
+			result = "";
+			start = e.startBit;
+			while (count) {
+				result = result.concat("", inputData.slice(start - 7, start + 1));
+				start += 8;
+				count -= 1;
+			}
+			const left = e.bitLength % 8;
+			result = result.concat("", inputData.slice(start - 7, start - 7 + left));
+
 			let slicedData = parseInt(result, 2);
-			// if (e.isSigned) {
-			// 	slicedData = ~~parseInt(result, 2);
-			// }
 
 			const signalName = e.name;
 			const signalFactor = e.factor;
@@ -52,5 +61,5 @@ function canParser(id, hexString) {
 }
 
 console.log(canParser(16, "40800000d17f0000"));
-// console.log(canParser(16, "0000006303f20546"));
-// console.log(canParser(18,"0d27000003de0186"))
+console.log(canParser(16, "0000006303f20546"));
+console.log(canParser(18, "0d27000003de0186"));
--- Fsae-Live-Telemetry-Dashboard/src/lib/test.js
+++ Fsae-Live-Telemetry-Dashboard/src/lib/test.js
@@ -10,9 +10,10 @@
 // 	];
 // }
 
-function hextobinary(data) {
-	return parseInt(data, 16).toString(2).padStart(8, "0");
-}
-const binary = "0100101";
-const hex = "AB";
-console.log(hextobinary(hex));
+// function hextobinary(data) {
+// 	return parseInt(data, 16).toString(2).padStart(8, "0");
+// }
+// const binary = "0100101";
+// const hex = "AB";
+// console.log(hextobinary(hex));
+console.log(Math.floor(15 / 8));
