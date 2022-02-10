82fe7804e1ca76073f3367f95184275c75ef5532
asanjay123 <achintya_s@outlook.com>
2022-02-10 17:44:12 -0600

    fix conflict

---
+++ Fsae-Live-Telemetry-Dashboard/src/components/DownloadLink.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Form from "react-bootstrap/Form";
+import { useData } from "../contexts/DataContext";
+
+
+const { currentData } = useData();
+
+
+// var data = [
+//     ['Data', currentData.data], 
+//     ['TimeStamp', currentData.],
+//     ['ID', currentData.],
+// ];
+
+function download_csv_file() {  
+  
+    var csv_file = 'Data,TimeStamp,ID\n';  
+      
+    data.forEach(function(row) {  
+            csv_file += row.join(',');  
+            csv_file += "\n";  
+    });  
+   
+    document.write(csv_file);  
+  
+    var hiddenElement = document.createElement('a');  
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv_file);  
+    hiddenElement.target = '_blank';  
+      
+    hiddenElement.download = 'Car Data';  
+    hiddenElement.click();  
+}  
