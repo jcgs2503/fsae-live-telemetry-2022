560de50a75f1ad955e9607cb15af5d371456f25d
Jerry Chang <jcgs2503@gmail.com>
2022-03-23 12:22:04 -0500

    pull newest version

--- Fsae-Live-Telemetry-Dashboard/src/lib/canParser.js
+++ Fsae-Live-Telemetry-Dashboard/src/lib/canParser.js
@@ -12,7 +12,6 @@ function reverseString(str) {
 
 function canParser(id, hexString) {
 	let inputData = hex2binary(hexString);
-	console.log(inputData);
 	const node = data.params.filter((e) => e.canId === id)[0];
 	const signals = node.signals.map((e) => e);
 
@@ -71,5 +70,5 @@ function canParser(id, hexString) {
 
 // console.log(canParser(16, "40800000d17f0000"));
 // console.log(canParser(16, "0000006303f20546"));
-// console.log(canParser(18, "0d27000003de0186"));
-console.log(canParser(17, "d20000d200e600dc"));
+console.log(canParser(18, "0d27000003de0186"));
+// console.log(canParser(17, "d20000d200e600dc"));
